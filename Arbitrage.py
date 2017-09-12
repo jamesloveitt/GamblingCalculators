3cde5439506d3f56214a36929cd26144c01faa24
jamesloveitt <jsloveitt@gmail.com>
2017-09-12 12:31:00 +0100

    Methods for X quantities
    Methods for X quantities of odds instead of 2 or 3

--- Arbitrage.py
+++ Arbitrage.py
@@ -1,16 +1,89 @@
 #!/usr/bin/python
+import math
 
-def percentTwoInput(a,b):
-	apercent = (1/a)*100
-	bpercent = (1/b)*100
+def percentXInput(oddslist):
+	sum = 0
+
+	for i in oddslist:
+		percent = (1/i)*100
+		sum = sum + percent
+		print(percent)
+
+	print(sum)
+	return sum
+
+def percentOutcome(sum):
+
+	profit = (100/sum)*100 
+
+	print(profit)
+	return profit
+
+def rawProfitPercent(sum):
+	percent = percentOutcome(sum)
+	raw = percent - 100
+	print(raw)
+	return raw
+
+
+def maxProfitValuesTwo(odds1,odds2,capital):
+	apercent = (1/odds1)*100
+	bpercent = (1/odds2)*100
+
+	sum = apercent + bpercent
 
 	print(apercent)
 	print(bpercent)
 
-	totalpercent = apercent + bpercent
-	print(totalpercent)
+	abet = (capital*apercent) / sum
+	bbet = (capital*bpercent) / sum
+
+	print(abet)
+	print(bbet)
 
 
-percentTwoInput(2.0,2.1)
+
+def maxProfitValuesX(oddslist,capital):
+	percentlist = []
+	sum = 0
+
+	for i in oddslist:
+		percent = (1/i)*100
+		percentlist.append(percent)
+		sum = sum + percent
+		print(percent)
+
+	print(sum)
+	betlist = []
+
+	for i in percentlist:
+		bet = (capital*i)/sum
+		betlist.append(bet)
+		print(bet)
+
+	return betlist
+
+
+def roundUpOneDec(bet1,bet2):
+	print(math.ceil(bet1))
+	print(math.ceil(bet2))
+
+def roundDownOneDec(bet1,bet2):
+	print(math.floor(bet1))
+	print(math.floor(bet2))
+
+def roundOneDec(bet1,bet2):
+	print(round(bet1))
+	print(round(bet2))
+
+
+
+maxProfitValuesX([3.3,3.0,2.9],1000)
+
+percentXInput([4.1,4.2,4.0,4.3])
+
+roundUpOneDec(96.342,71.104)
+    
+
 
 
