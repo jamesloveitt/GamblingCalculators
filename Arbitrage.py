86c9d9d4f5f88c6abb52af308c09c10fdfb30692
jamesloveitt <jsloveitt@gmail.com>
2017-09-16 22:02:24 +0100

    Further attempts at clean rounding + clean up
    Attempting to make a cleaner solution to rounding stakes and the clean
    up or removal of some existing methods

--- Arbitrage.py
+++ Arbitrage.py
@@ -7,22 +7,19 @@ def percentXInput(oddslist):
 	for i in oddslist:
 		percent = (1/i)*100
 		sum = sum + percent
-		print(percent)
 
-	print(sum)
 	return sum
 
 def percentOutcome(sum):
 
 	profit = (100/sum)*100 
 
-	print(profit)
 	return profit
 
-def rawProfitPercent(sum):
+def rawProfitPercent(oddslist):
+	sum = percentXInput(oddslist)
 	percent = percentOutcome(sum)
 	raw = percent - 100
-	print(raw)
 	return raw
 
 
@@ -32,14 +29,10 @@ def maxProfitValuesTwo(odds1,odds2,capital):
 
 	sum = apercent + bpercent
 
-	print(apercent)
-	print(bpercent)
-
 	abet = (capital*apercent) / sum
 	bbet = (capital*bpercent) / sum
 
-	print(abet)
-	print(bbet)
+	return(abet,bbet)
 
 
 
@@ -51,30 +44,33 @@ def maxProfitValuesX(oddslist,capital):
 		percent = (1/i)*100
 		percentlist.append(percent)
 		sum = sum + percent
-		print(percent)
 
-	print(sum)
 	betlist = []
 
 	for i in percentlist:
 		bet = (capital*i)/sum
 		betlist.append(bet)
-		print(bet)
 
 	return betlist
 
 
 def roundUpOneDec(bet1,bet2):
-	print(math.ceil(bet1))
-	print(math.ceil(bet2))
+	ceil1 = (math.ceil(bet1))
+	ceil2 = (math.ceil(bet2))
+
+	return(ceil1,ceil2)
 
 def roundDownOneDec(bet1,bet2):
-	print(math.floor(bet1))
-	print(math.floor(bet2))
+	floor1 = (math.floor(bet1))
+	floor2 = (math.floor(bet2))
+
+	return(floor1,floor2)
 
 def roundOneDec(bet1,bet2):
-	print(round(bet1))
-	print(round(bet2))
+	round1 = (round(bet1))
+	round2 = (round(bet2))
+
+	return(round1,round2)
 
 
 def suggestedBets(odds1,odds2,capital):
@@ -82,7 +78,6 @@ def suggestedBets(odds1,odds2,capital):
 	a = (odds1*100) + (odds2*100)
 	b = capital/a
 	c = math.floor(b)
-	print(c)
 	sugodds1 = (100*c*odds2)
 	sugodds2 = (100*c*odds1)
 	print("Suggested bet for odds1:%f" % sugodds1)
@@ -92,71 +87,78 @@ def suggestedBets(odds1,odds2,capital):
 def modTenRec(odds1,odds2):
 	a = (odds1*100)
 	b = (odds2*100)
-	print(a%10)
-	print(b%10)
 
 	if a % 10 == 0 and b % 10 == 0:
 		a = a/10
 		b = b/10
-		print(a)
-		print(b)
 		return modTenRec(a/100,b/100)
 	else:
-		print(a)
-		print(b)
-
-	return (a,b)
+		return (a,b)
 
 def modXRec(odds1,odds2,mod):
-	a = (odds1*100)
-	b = (odds2*100)
-	print(a%mod)
-	print(b%mod)
+	oddsone = odds1*100
+	oddstwo = odds2*100
 
-	if a % mod == 0 and b % mod == 0:
-		a = a/mod
-		b = b/mod
-		print(a)
-		print(b)
-		return modXRec(a/100,b/100,mod)
+	mod1 = oddsone % mod
+	mod2 = oddstwo % mod
+
+	if odds1 == odds2 and mod1 == 0 :
+		return (1,1)
+	elif odds1 == odds2:
+		return (oddsone,oddstwo)
+	elif mod1 == 0 and mod2 == 0:
+		return modXRec((oddsone/mod)/100,(oddstwo/mod)/100,mod)
 	else:
-		print(a)
-		print(b)
+		return (oddsone,oddstwo)
 
-	return (a,b)
+
+def modXRec2(oddslist,mod):
+	expanded = [100*x for x in oddslist]
+
+	modlist = [x % mod for x in expanded]
+
+	retlist = []
+
+	if all(x == oddslist[0] for x in oddslist) and modlist[0] == 0:
+		for i in oddslist:
+			retlist.append(1)
+		return retlist
+	else:
+		return null
+
 
 def suggestedBetsV2(odds1,odds2,capital,mod):
+
+	print("The potential profit on this bet is %f%%" % rawProfitPercent([odds1,odds2]))
+
+	profitlist = maxProfitValuesX([odds1,odds2],capital)
+
+	print("The ideal betting values for maximum profit would be %f and %f" % (profitlist[0], profitlist[1]))
+
 	a = modXRec(odds1,odds2,mod)
 	b = a[0] + a[1]
-	print(a[0])
-	print(a[1])
-
 	c = capital/b
-	print(c)
-	d = c - c % mod
-	print(d)
-	sugodds1 = (a[0]*d)
-	sugodds2 = (a[1]*d)
-	print("Suggested bet for odds1:%f" % sugodds1)
-	print("Suggested bet for odds2:%f" % sugodds2)	
+	d = c - (c % mod)
+	if d == 0:
+		print("You do not have sufficient Capital to make clean bets that round to %d and that do not sacrifice your profit margin" % mod)
+		return (0,0)
+	else:
+		sugodds1 = (a[0]*d)
+		sugodds2 = (a[1]*d)
+		print("Suggested bet for odds1:%f" % sugodds1)
+		print("Suggested bet for odds2:%f" % sugodds2)
+		return (sugodds1,sugodds2)	
+
+suggestedBetsV2(3.0,1.57,1000,5)
+
+def suggestedBetsX(oddslist,capital,mod):
+	print("The potential profit on this bet is %f%%" % rawProfitPercent(oddslist))
+
+	profitlist = maxProfitValuesX(oddslist,capital)
+
+	print("The ideal betting values for maximum profit would be:" + ",".join(map(str,profitlist)))
 
 
 
-#maxProfitValuesX([3.3,3.0,2.9],1000)
 
-#percentXInput([4.1,4.2,4.0,4.3])
-
-#roundUpOneDec(96.342,71.104)
-    
-#suggestedBets(2.1,2.0,1000)
-
-#modTenRec(2.1,2.0)
-#modTenRec(2.23,2.41)
-
-#modXRec(2.1,2.0,5)
-#modXRec(2.1,2.0,10)
-#modXRec(2.1,2.0,20)
-#modXRec(2.1,2.0,2)
-#modXRec(2.1,2.0,4)
-
-suggestedBetsV2(2.0,2.0,2500,5)
+suggestedBetsX([3.1,3.0,3.2],5000,10)
